c3274b4389eae780e1988364cc278c84eff3d68e
Adriano Rocha De Carvalho Filho <adrocha-@c2r3s2.42lisboa.com>
2026-03-13 21:36:59 +0000

    s

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: adrocha- <adrocha-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 22:05:21 by adrocha-          #+#    #+#             */
-/*   Updated: 2026/03/12 20:41:08 by adrocha-         ###   ########.fr       */
+/*   Updated: 2026/03/13 20:40:48 by adrocha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # include <string.h>
 # include <unistd.h>
 
+typedef struct s_philo t_philo;
+
 typedef struct s_table
 {
 	int				num_of_philo;
@@ -26,20 +28,20 @@ typedef struct s_table
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				must_eat_count;
-	// t_philo			*philos;
-	// pthread_mutex_t	*forks;
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
 }					t_table;
 
-// typedef struct s_philo
-// {
-// 	int				id;
-// 	pthread_t		thread;
-// 	int				left_fork;
-// 	int				right_fork;
-// 	long			last_meal;
-// 	int				meals_eaten;
-// 	t_table			*table;
-// }					t_philo;
+typedef struct s_philo
+{
+	int				id;
+	pthread_t		thread;
+	int				left_fork;
+	int				right_fork;
+	long			last_meal;
+	int			meals_eaten;
+	t_table			*table;
+}					t_philo;
 
 int					ft_atoi(const char *str);
 int					ft_isnum(int c);
--- src/main.c
+++ src/main.c
@@ -6,87 +6,53 @@
 /*   By: adrocha- <adrocha-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 20:19:24 by adrocha-          #+#    #+#             */
-/*   Updated: 2026/03/12 20:39:58 by adrocha-         ###   ########.fr       */
+/*   Updated: 2026/03/13 21:20:03 by adrocha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-// int	ft_atoi(const char *str)
-// {
-// 	int	i;
-// 	int	number;
-// 	int	sign;
+void	init_table(t_table *table, int ac, char *av[])
+{
+	table->num_of_philo = atoi(av[1]);
+	table->time_to_die = atoi(av[2]);
+	table->time_to_eat = atoi(av[3]);
+	table->time_to_sleep = atoi(av[4]);
+	// table->must_eat_count = mec;
+	table->philos = malloc((table->num_of_philo) * sizeof(t_philo));
+	if (!table->philos)
+		return ;
+	table->forks = malloc((table->num_of_philo) * sizeof(pthread_mutex_t));
+	if (!table->forks)
+	{
+		free(table->philos);
+		return ;
+	}
+	return ;
+}
 
-// 	sign = 1;
-// 	number = 0;
-// 	i = 0;
-// 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-// 		i++;
-// 	if (str[i] == '+' || str[i] == '-')
-// 	{
-// 		if (str[i] == '-')
-// 			sign = -sign;
-// 		i++;
-// 	}
-// 	while (str[i] >= '0' && str[i] <= '9')
-// 	{
-// 		number *= 10;
-// 		number += str[i] - '0';
-// 		i++;
-// 	}
-// 	return (number * sign);
-// }
+void init_philos(t_table *table)
+{
+	
+	int i = 0;
 
-// int	ft_isnum(int c)
-// {
-// 	return (c >= '0' && c <= '9');
-// }
-
-// int	parsing(int ac, char *av[])
-// {
-// 	int	i;
-// 	int	c;
-// 	int	j;
-
-// 	if (ac < 5 || ac > 6)
-// 		return (0);
-// 	i = 1;
-// 	while (i < ac)
-// 	{
-// 		j = 0;
-// 		while (av[i][j])
-// 		{
-// 			if (!ft_isnum(av[i][j]))
-// 				return (0);
-// 			j++;
-// 		}
-// 		c = ft_atoi(av[i]);
-// 		if (c <= 0)
-// 			return (0);
-// 		i++;
-// 	}
-// 	return (1);
-// }
+	while()
+	{
+		
+	}
+	
+}
 
 int	main(int ac, char *av[])
 {
-	static t_table	c;
+	static t_table	table;
 
 	if (!parsing(ac, av))
 	{
 		printf("Error:\n");
 		return (1);
 	}
-	printf("input valido\n");
-	c.num_of_philo = atoi(av[1]);
-	c.time_to_die = atoi(av[2]);
-	c.time_to_eat = atoi(av[3]);
-	c.time_to_sleep = atoi(av[4]);
-	// c.must_eat_count = mec;
-	printf("Esse é o numero de filosofos na mesa:%i\n", c.num_of_philo);
-	printf("Hora de morrer:%i\n", c.time_to_die);
-	printf("Hora de comer:%i\n", c.time_to_eat);
-	printf("Hora de dormir:%i\n", c.time_to_sleep);
+	init_table(&table, ac, av);
+	init_philos();
 	return (0);
 }
