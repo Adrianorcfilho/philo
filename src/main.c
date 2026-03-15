f0ab231c8d013c4ea49bdc6e64f68e81b1ee7e16
Adrianorcfilho <Adrianorcfilho@users.noreply.github.com>
2026-03-15 13:13:42 +0000

    t

--- src/main.c
+++ src/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adrocha- <adrocha-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adrocha <adrocha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 20:19:24 by adrocha-          #+#    #+#             */
-/*   Updated: 2026/03/13 21:20:03 by adrocha-         ###   ########.fr       */
+/*   Updated: 2026/03/15 13:03:25 by adrocha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-void	init_table(t_table *table, int ac, char *av[])
+void	init_table(t_table *table, char *av[])
 {
 	table->num_of_philo = atoi(av[1]);
 	table->time_to_die = atoi(av[2]);
@@ -36,10 +36,17 @@ void init_philos(t_table *table)
 	
 	int i = 0;
 
-	while()
+	while(i < table->num_of_philo)
 	{
-		
+		table->philos[i].id = i + 1;
+		table->philos[i].left_fork = i;
+		table->philos[i].right_fork = ((i + 1) % table->num_of_philo);
+		table->philos[i].table = table;
+		table->philos[i].meals_eaten = 0;
+		table->philos[i].last_meal = 0;
+		i++;
 	}
+	return ;
 	
 }
 
@@ -52,7 +59,7 @@ int	main(int ac, char *av[])
 		printf("Error:\n");
 		return (1);
 	}
-	init_table(&table, ac, av);
-	init_philos();
+	init_table(&table, av);
+	init_philos(&table);
 	return (0);
 }
